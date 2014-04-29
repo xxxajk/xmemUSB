64046e653e7ca53b852bfa3746d869f50f2f7548
Andrew J. Kroll <a@oo.ms>
2014-04-29 06:02:39 -0400

    teensy 3.x support via ISR

--- xmemUSB.cpp
+++ xmemUSB.cpp
@@ -1,45 +1,86 @@
-#include <xmem.h>
 #include <xmemUSB.h>
-#include <Usb.h>
-#include <stdio.h>
 
-extern "C" unsigned int freeHeap();
-
-#define MAX_USB_MODULES 10
 extern USB Usb;
 
 volatile USB_Module_Calls USB_Module[MAX_USB_MODULES + 1] = {NULL};
 
-// USB task
-
-void USB_main(void) {
-
-        uint8_t last_state = 1;
-        uint8_t current_state;
-        uint8_t i = 0;
-        while (USB_Module[i]) {
-                if (!USB_Module[i](1, 0, 0)) return; // Runs Init
-                i++;
-        }
-
-        while (Usb.Init(1000) == -1);
-
-        for (;;) {
-                Usb.Task();
-                current_state = Usb.getUsbTaskState();
-                i = 0;
-                while (USB_Module[i]) {
-                        if (!USB_Module[i](2, current_state, last_state)) xmem::Yield(); //Poll, sleep if work is done
-                        i++;
-                }
-                last_state = current_state;
-
-        }
-}
-
+#if defined(__AVR__)
+extern "C" unsigned int freeHeap();
 static FILE tty_stdio;
 static FILE tty_stderr;
 static uint8_t PID_of_USB;
+#elif defined(__arm__) && defined(CORE_TEENSY)
+#include <sys/stat.h>
+#define USB_TASK_INTERVAL 10000
+static IntervalTimer USB_timed_isr;
+static uint8_t last_state = 1;
+static uint8_t current_state;
+
+// USB task isr
+
+void Do_USB(void) {
+        USB_ISR_PROTECTED_CALL() {
+                int i;
+                // These can take a while...
+                Usb.Task();
+                current_state = Usb.getUsbTaskState();
+                i = 0;
+                while(USB_Module[i]) {
+                        USB_Module[i](2, current_state, last_state); // Runs each module
+                        i++;
+                }
+                last_state = current_state;
+        }
+
+}
+
+uint8_t USB_ISR_PROTECTED_CALL_START() {
+        USB_timed_isr.end();
+        return 1;
+}
+
+uint8_t USB_ISR_PROTECTED_CALL_END() {
+        USB_timed_isr.begin(Do_USB, USB_TASK_INTERVAL);
+        return 0;
+}
+
+
+#endif
+
+// USB task for avr, setup-completion for teensy 3.x
+
+void USB_main(void) {
+        uint8_t i = 0;
+#if  defined(__arm__) && defined(CORE_TEENSY)
+
+        USB_ISR_PROTECTED_CALL() {
+#endif
+                while(USB_Module[i]) {
+                        if(!USB_Module[i](1, 0, 0)) return; // Runs Init on each
+                        i++;
+                }
+
+                while(Usb.Init(1000) == -1);
+#if  defined(__arm__) && defined(CORE_TEENSY)
+        //                USB_timed_isr.begin(Do_USB, USB_TASK_INTERVAL); // .01 second
+
+        }
+#else
+                for(;;) {
+                        Usb.Task();
+                        current_state = Usb.getUsbTaskState();
+                        i = 0;
+                        while(USB_Module[i]) {
+                                if(!USB_Module[i](2, current_state, last_state)) xmem::Yield(); //Poll, sleep if work is done
+                                i++;
+                        }
+                        last_state = current_state;
+
+                }
+#endif
+}
+
+#if defined(__AVR__)
 
 // Fun! Serial stuff is thread safe!
 
@@ -55,15 +96,52 @@ static int tty_std_putc(char c, FILE *t) {
 }
 
 static int tty_std_getc(FILE *t) {
-        while (!KONSOLE.available());
+        while(!KONSOLE.available());
         return KONSOLE.read();
 }
+#else
+extern "C" {
+
+        int _write(int fd, const char *ptr, int len) {
+                int j;
+                for(j = 0; j < len; j++) {
+                        if(fd == 1)
+                                KONSOLE.write(*ptr++);
+                        else if(fd == 2)
+                                USB_HOST_SERIAL.write(*ptr++);
+                }
+                return len;
+        }
+
+        int _read(int fd, char *ptr, int len) {
+                if(len > 0 && fd == 0) {
+                        while(!KONSOLE.available());
+                        *ptr = KONSOLE.read();
+                        return 1;
+                }
+                return 0;
+        }
+
+        int _fstat(int fd, struct stat *st) {
+                memset(st, 0, sizeof (*st));
+                st->st_mode = S_IFCHR;
+                st->st_blksize = 1024;
+                return 0;
+        }
+
+        int _isatty(int fd) {
+                return (fd < 3) ? 1 : 0;
+        }
+}
+#endif
 
 void USB_Setup(USB_Module_Calls func[]) {
 
 #if CONSOLE == 0
         while(!KONSOLE);
 #endif
+        delay(100); // Stupid, but needed (argh!)
+#if defined(__AVR__)
         // Set up stdio/stderr
         tty_stdio.put = tty_std_putc;
         tty_stdio.get = tty_std_getc;
@@ -77,23 +155,27 @@ void USB_Setup(USB_Module_Calls func[]) {
         tty_stderr.flags = _FDEV_SETUP_WRITE;
         tty_stderr.udata = 0;
         stderr = &tty_stderr;
-
-                KONSOLE.begin(CONSOLE_BAUD_RATE);
+#elif defined(__arm__) && defined(CORE_TEENSY)
+        //setbuffer(stdout, NULL, 0);
+        //setvbuf (stdout, NULL, _IONBF, 0);
+#endif
+        KONSOLE.begin(CONSOLE_BAUD_RATE);
 
 #if USB_HOST_SERIAL_NUM != CONSOLE
-                USB_HOST_SERIAL.begin(CONSOLE_BAUD_RATE);
+        USB_HOST_SERIAL.begin(CONSOLE_BAUD_RATE);
 #endif
 
         uint8_t i = 0;
-        while (func[i]) {
+        while(func[i]) {
                 USB_Module[i] = func[i];
-                if (!USB_Module[i](0, 0, 0)) return; // Runs Setup on each
+                if(!USB_Module[i](0, 0, 0)) return; // Runs Setup on each
                 i++;
-                if (i == MAX_USB_MODULES) {
+                if(i == MAX_USB_MODULES) {
                         break;
                 }
         }
         USB_Module[i] = NULL;
+#if defined(__AVR__)
         PID_of_USB = xmem::SetupTask(USB_main
 #ifdef _MAX_SS
 #if _MAX_SS != 512
@@ -101,6 +183,6 @@ void USB_Setup(USB_Module_Calls func[]) {
 #endif
 #endif
                 );
-        if (PID_of_USB) xmem::StartTask(PID_of_USB);
+        if(PID_of_USB) xmem::StartTask(PID_of_USB);
+#endif
 }
-
--- xmemUSB.h
+++ xmemUSB.h
@@ -7,6 +7,13 @@
 
 #ifndef XMEMUSB_H
 #define	XMEMUSB_H
+#include <xmem.h>
+#include <Usb.h>
+#include <stdio.h>
+
+#ifndef MAX_USB_MODULES
+#define MAX_USB_MODULES 10
+#endif
 
 #ifndef CONSOLE
 #define CONSOLE 0
@@ -29,7 +36,9 @@
 typedef int (*USB_Module_Calls) (uint8_t, uint8_t, uint8_t);
 extern void USB_Setup(USB_Module_Calls func[]);
 extern void USB_main(void);
-
-
+#if  defined(__arm__) && defined(CORE_TEENSY)
+uint8_t USB_ISR_PROTECTED_CALL_START();
+uint8_t USB_ISR_PROTECTED_CALL_END();
+#define USB_ISR_PROTECTED_CALL() for(char __ToDo = USB_ISR_PROTECTED_CALL_START(); __ToDo;  __ToDo = USB_ISR_PROTECTED_CALL_END())
+#endif
 #endif	/* XMEMUSB_H */
-
